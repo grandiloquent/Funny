8c20a9fca8bf0824f3e321ec99e93bbec0b3cc50
grandiloquent <1785907865@qq.com>
2019-01-18 00:32:21 +0800

    changed

--- app/src/main/assets/server/editor.js
+++ app/src/main/assets/server/editor.js
@@ -13,13 +13,54 @@
             // https://github.com/sparksuite/simplemde-markdown-editor
             this.mde = new SimpleMDE({
                 element: this.element,
-                hideIcons: ["guide"]
+                hideIcons: ["guide"],
+                spellChecker: false
+
             });
+            this.mde.codemirror.addKeyMap({
+                "F1": () => this.onUpdate(),
+                "Esc": () => {
+                    window.location.hash = "";
+                }
+            })
 
             window.onhashchange = this.onHashChange.bind(this);
         }
     };
-
+    Editor.prototype.onUpdate = function () {
+        var obj = this.collect();
+        if (!obj) return;
+        fetch("/api/update", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+                body: JSON.stringify(obj)
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+            });
+    }
+    Editor.prototype.collect = function () {
+        var obj = {};
+        var hash = window.location.hash;
+        if (hash.length < 1) return;
+        hash = hash.substring(1);
+        obj['id'] = parseInt(hash) || 0;
+        var value = this.mde.value().trim();
+        if (value.length < 1) return null;;
+        var p = value.indexOf('\n');
+        var title = "";
+        if (p != -1) {
+            title = value.substring(0, p).trim();
+        } else {
+            title = value;
+        }
+        obj['title'] = title;
+        obj['content'] = value;
+        return obj;
+    }
     Editor.prototype.onFetch = function (hash) {
         fetch("/api/get/" + hash, {
                 method: "POST",
@@ -29,14 +70,15 @@
             })
             .then(response => response.json())
             .then(data => {
-                let value = JSON.stringify(data)['content'];
+
+                let value = data['content'];
+                console.log(value);
                 this.mde.value(value);
             });
     }
     Editor.prototype.onHashChange = function (e) {
         var hash = window.location.hash;
         this.onFetch(hash.substring(1));
-        console.log(e);
     };
     window["Editor"] = Editor;
 
