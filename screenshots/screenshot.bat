0ae5dc54063a4ae6b9001f35f48327457477d522
grandiloquent <1785907865@qq.com>
2019-01-18 03:08:39 +0800

    Update Screenshot

---
+++ screenshots/screenshot.bat
@@ -0,0 +1,3 @@
+C:\Users\psycho\AppData\Local\Android\Sdk\platform-tools\adb shell screencap -p /sdcard/screen.png
+C:\Users\psycho\AppData\Local\Android\Sdk\platform-tools\adb pull /sdcard/screen.png
+C:\Users\psycho\AppData\Local\Android\Sdk\platform-tools\adb shell rm /sdcard/screen.png
